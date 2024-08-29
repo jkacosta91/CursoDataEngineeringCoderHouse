a534e71e36f5e6a997b3b7a4acfbde8974a8be2b
Jonathan Acosta <jkacosta91@gmail.com>
2024-08-29 21:27:41 +0200

    1

--- airflow/dags/modules/__init__.py
+++ airflow/dags/modules/__init__.py
@@ -1,4 +1,2 @@
-from .utils import get_defaultairflow_args
-from .data_extract import extraer_data
-from .data_load import cargar_data
-from .data_transform import transformar_data
+from .data_con import DataConn
+from .get_data_from_api import DataRetriever
--- modules/__init__.py
+++
@@ -1,2 +0,0 @@
-from .data_con import DataConn
-from .get_data_from_api import DataRetriever
