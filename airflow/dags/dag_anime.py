d3f4f413b3ba065a295363b5c057f7a587c61431
Jonathan Acosta <jkacosta91@gmail.com>
2024-08-30 18:41:11 +0200

    entregable 3

--- airflow/dags/dag_anime.py
+++ airflow/dags/dag_anime.py
@@ -1,54 +1,76 @@
 from datetime import datetime, timedelta
-from email import message
 from airflow import DAG
-from airflow.models import Variable
 from airflow.operators.bash import BashOperator
-from airflow.operators.python_operator import PythonOperator
+from airflow.operators.python import PythonOperator
+from modules.get_data_from_api import DataRetriever
+from modules.data_con import DataConn
+import os
 
-
-from modules import get_data_from_api, data_con
-
-default_args={
-    'owner': 'JKACOSTA',
-    'retries': 5,
-    'retry_delay': timedelta(minutes=2) # 2 min de espera antes de cualquier re intento
+# Cargar las variables de entorno
+config = {
+    'REDSHIFT_USERNAME': os.getenv('REDSHIFT_USERNAME'),
+    'REDSHIFT_PASSWORD': os.getenv('REDSHIFT_PASSWORD'),
+    'REDSHIFT_HOST': os.getenv('REDSHIFT_HOST'),
+    'REDSHIFT_PORT': os.getenv('REDSHIFT_PORT', '5439'),
+    'REDSHIFT_DBNAME': os.getenv('REDSHIFT_DBNAME')
 }
 
-DAG_ID = "postgres_operator_dag"
+# Definir argumentos por defecto
+default_args = {
+    'owner': 'JKACOSTA',
+    'retries': 5,
+    'retry_delay': timedelta(minutes=2)
+}
 
-with DAG(
+# Crear el DAG
+dag = DAG(
     dag_id="desafio3_pipeline",
-    default_args= default_args,
+    default_args=default_args,
     description="DAG para consumir API y vaciar datos en Redshift",
-    start_date=datetime(2024,8,1),
+    start_date=datetime(2024, 8, 1),
     schedule_interval='@daily',
     catchup=False
-    ) as dag:
+)
 
-    # Tasks
-    # 1. 
-    task_1 = BashOperator(
-        task_id='primera_tarea',
-        bash_command='echo Iniciando...'
-        )
-    
-    # 2.
-    task_2 = PythonOperator(
-        task_id="get_data_from_api",
-        python_callable=get_data_from_api
-        )
+def retrieve_data():
+    retriever = DataRetriever()
+    data = retriever.get_data(page=1, limit=25)
+    return data
 
-    # 3. 
-    task_3 = PythonOperator(
-        task_id="data_con",
-        python_callable=data_con
-        )
+def upload_data(**context):
+    data = context['ti'].xcom_pull(task_ids='get_data_from_api')
+    if not data.empty:
+        schema = os.getenv('REDSHIFT_SCHEMA')
+        table = os.getenv('REDSHIFT_TABLE')
+        conn = DataConn(config=config, schema=schema)
+        conn.upload_data(data, table)
 
-    # 4. Loading
-    task_4 = BashOperator(
-        task_id= 'tercera_tarea',
-        bash_command='echo Proceso completado...'
-    )
+# Definir las tareas
+task_1 = BashOperator(
+    task_id='primera_tarea',
+    bash_command='echo Iniciando...',
+    dag=dag
+)
 
-    # Task dependencies
-    task_1 >> task_2 >> task_3 >> task_4
+task_2 = PythonOperator(
+    task_id='get_data_from_api',
+    python_callable=retrieve_data,
+    provide_context=True,
+    dag=dag
+)
+
+task_3 = PythonOperator(
+    task_id='upload_data_to_redshift',
+    python_callable=upload_data,
+    provide_context=True,
+    dag=dag
+)
+
+task_4 = BashOperator(
+    task_id='tercera_tarea',
+    bash_command='echo Proceso completado...',
+    dag=dag
+)
+
+# Definir dependencias
+task_1 >> task_2 >> task_3 >> task_4
